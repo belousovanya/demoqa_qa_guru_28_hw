1e415d46bf08c24cbcf22897a57e2d143d44487a
Anna Belousova <belusovanna@gmail.com>
2024-06-30 19:11:58 +0500

    added OutputTextBoxComponent

---
+++ src/test/java/pages/components/OutputTextBoxComponent.java
@@ -0,0 +1,33 @@
+package pages.components;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class OutputTextBoxComponent {
+    public SelenideElement userNameOutput = $("#output #name"),
+            userEmailOutput = $("#output #email"),
+            currentAddressOutput = $("#output #currentAddress"),
+            permanentAddressOutput = $("#output #permanentAddress");
+
+    public OutputTextBoxComponent checkUserNameOutput(String value) {
+        userNameOutput.shouldHave(text(value));
+        return this;
+    }
+
+    public OutputTextBoxComponent checkUserEmailOutput(String value) {
+        userEmailOutput.shouldHave(text(value));
+        return this;
+    }
+
+    public OutputTextBoxComponent checkCurrentAddressOutput(String value) {
+        currentAddressOutput.shouldHave(text(value));
+        return this;
+    }
+
+    public OutputTextBoxComponent checkPermanentAddressOutput(String value) {
+        permanentAddressOutput.shouldHave(text(value));
+        return this;
+    }
+}
