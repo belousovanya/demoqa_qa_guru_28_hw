5d99194165f221ed9d8c58e3151ce6871b548002
Anna Belousova <belusovanna@gmail.com>
2024-06-30 18:02:20 +0500

    added StudentRegistrationFormWithPageObjectsTests

---
+++ src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
@@ -0,0 +1,69 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import pages.components.ResultTableComponent;
+
+public class StudentRegistrationFormWithPageObjectsTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+    ResultTableComponent resultTableComponent = new ResultTableComponent();
+
+    @Test
+    public void successfulRegistrationTest() {
+        registrationPage.openRegistrationPage()
+                .setFirstNameInput("Anna")
+                .setLastNameInput("Bel")
+                .setUserEmailInput("example@example.com")
+                .setGenderWrapper("Female")
+                .setUserNumberInput("7999999999")
+                .setDateOfBirthInput("23", "August", "1985")
+                .setSubjectsInput("Maths")
+                .setHobbiesWrapper("Reading")
+                .setUploadPicture("photo.jpg")
+                .setCurrentAddressInput("Some street 1")
+                .setState("Uttar Pradesh")
+                .setCity("Agra")
+                .clickSubmitButton();
+
+        resultTableComponent.checkResultHeader("Thanks for submitting the form")
+                .checkResultTable("Student Name", "Anna Bel")
+                .checkResultTable("Student Email", "example@example.com")
+                .checkResultTable("Gender", "Female")
+                .checkResultTable("Mobile", "7999999999")
+                .checkResultTable("Date of Birth", "23 August,1985")
+                .checkResultTable("Subjects", "Maths")
+                .checkResultTable("Hobbies", "Reading")
+                .checkResultTable("Picture", "photo.jpg")
+                .checkResultTable("Address", "Some street 1")
+                .checkResultTable("State and City", "Uttar Pradesh Agra");
+    }
+
+    @Test
+    public void successfulRegistrationUsingRequiredFieldsTest() {
+        registrationPage.openRegistrationPage()
+                .setFirstNameInput("Anna")
+                .setLastNameInput("Bel")
+                .setGenderWrapper("Female")
+                .setUserNumberInput("7999999999")
+                .clickSubmitButton();
+
+        resultTableComponent.checkResultHeader("Thanks for submitting the form")
+                .checkResultTable("Student Name", "Anna Bel")
+                .checkResultTable("Gender", "Female")
+                .checkResultTable("Mobile", "7999999999");
+    }
+
+    @Test
+    public void RegistrationWithoutFirstNameTest() {
+        registrationPage.openRegistrationPage()
+                .setLastNameInput("Bel")
+                .setGenderWrapper("Female")
+                .setUserNumberInput("7999999999")
+                .clickSubmitButton();
+
+        resultTableComponent.tableDoesntExist();
+
+    }
+}
+
