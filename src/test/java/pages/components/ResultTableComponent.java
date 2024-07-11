64fbaae49c56c6b75eb0962149cf8bfe3d1a59d6
Anna Belousova <belusovanna@gmail.com>
2024-07-12 01:50:12 +0500

    Rollback to previous code

--- src/test/java/pages/components/ResultTableComponent.java
+++ src/test/java/pages/components/ResultTableComponent.java
@@ -1,37 +1,102 @@
-package pages.components;
+package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class ResultTableComponent {
-    private final SelenideElement resultHeader = $("#example-modal-sizes-title-lg"),
-            resultTable = $(".table-responsive");
+public class RegistrationPage {
+    public SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            dateOfBirth = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            uploadPicture = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            state = $("#state"),
+            stateCityWrapper = $("#stateCity-wrapper"),
+            city = $("#city"),
+            submitButton = $("#submit");
 
-    public ResultTableComponent checkResultHeader(String value) {
-        resultHeader.shouldHave(exactText(value));
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    public RegistrationPage openRegistrationPage() {
+        open("/automation-practice-form");
         return this;
     }
 
-    public ResultTableComponent checkResultTable(String key, String value) {
-        if (key.equals("Date of Birth")) {
-            resultTable.$(byText(key)).sibling(0).shouldHave(exactText(formatDateForChecking(value)));
-        } else {
-            resultTable.$(byText(key)).sibling(0).shouldHave(exactText(value));
-        }
+    public RegistrationPage setFirstNameInput(String value) {
+        firstNameInput.setValue(value);
         return this;
     }
 
-    public ResultTableComponent tableDoesntExist() {
-        resultTable.shouldNotBe(exist);
+    public RegistrationPage setLastNameInput(String value) {
+        lastNameInput.setValue(value);
         return this;
     }
 
-    private String formatDateForChecking(String date) {
-        String[] dateParts = date.split(" ");
-        return dateParts[0] + " " + dateParts[1] + "," + dateParts[2];
+    public RegistrationPage setUserEmailInput(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setGenderWrapper(String value) {
+        genderWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setUserNumberInput(String value) {
+        userNumberInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirthInput(String day, String month, String year) {
+        dateOfBirth.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+    public RegistrationPage setSubjectsInput(String value) {
+        subjectsInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setHobbiesWrapper(String value) {
+        hobbiesWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setUploadPicture(String value) {
+        uploadPicture.uploadFromClasspath(value);
+        return this;
+
+    }
+
+    public RegistrationPage setCurrentAddressInput(String value) {
+        currentAddressInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setState(String value) {
+        state.click();
+        stateCityWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setCity(String value) {
+        city.click();
+        stateCityWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage clickSubmitButton() {
+        submitButton.click();
+        return this;
     }
 }
