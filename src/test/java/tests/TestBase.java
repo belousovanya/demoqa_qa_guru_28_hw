2219e212d74f481a7451c6f98eb8439545f90571
Anna Belousova <belusovanna@gmail.com>
2024-07-28 14:53:11 +0500

    added Configuration.headless and Configuration.browser

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,5 +9,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.headless = true;
+        Configuration.browser = "chrome";
     }
 }
