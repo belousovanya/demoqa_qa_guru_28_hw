8e79b9cc5535e796dc6712f1c0ba656b68bce666
Anna Belousova <belusovanna@gmail.com>
2024-07-28 15:50:37 +0500

    added selenide Listener

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -10,6 +12,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
     }
 }
