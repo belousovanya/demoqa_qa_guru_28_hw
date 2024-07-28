d7c78b4e541f0cf4b9f1432497f7a5ce0e89f0db
Anna Belousova <belusovanna@gmail.com>
2024-07-28 16:11:21 +0500

    added browserCapabilities

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,6 +6,9 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
@@ -15,6 +18,13 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
     }
 
     @AfterEach
